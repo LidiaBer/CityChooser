019ebc727bbc8ebab79f0da52f188a165264a372
LidiaBer <lytdia@hotmail.com>
2016-03-25 23:51:39 +0100

    Added files via upload

---
+++ vista.php
@@ -0,0 +1,22 @@
+<?php
+function insertaHtml(){
+	echo"<!DOCTYPE html>
+			<html lang='en'>
+			<head>
+    			<meta charset='UTF-8'>
+    			<title>CityChooser Administrador</title>
+			</head>
+			<link rel='stylesheet' href='main2.css'>
+			<nav>
+				<ul>
+					<li><a href='index.php'>Inicio</a></li>
+					<li><a href='ciudad.php'>Ciudad</a></li>
+					<li><a href='agencia.php'>Agencia</a></li>
+					<li><a href='usuario.php'>Usuario</a></li>
+				</ul>
+			</nav>
+			<section>
+			<img src='../imagenes/tierra_de_corazon.jpg' width='50%'>
+			</section>";
+	}
+?>
